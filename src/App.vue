8f1188abdd895461da60ddfa9909e66140b3a6ac
codwerk <louis_widdy@live.fr>
2020-10-31 09:29:48 +0100

    installation de vue et bulma, components: GridColumns

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GridColumns></GridColumns>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GridColumns from './components/GridColumns.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GridColumns
   }
 }
 </script>
---
+++ src/components/GridColumns.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- product info -->
+  <section class="section">
+    
+  </section>
+</template>
+
+<script>
+ export default {
+   data: () => {
+     return {}
+   }
+  }
+</script>
+
+<style>
+  
+</style>
