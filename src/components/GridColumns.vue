937bb78b362f4d2fd606650b2d9dc86f4f266e15
codwerk <louis_widdy@live.fr>
2020-10-31 13:43:48 +0100

    comment utiliser le système de grille Bulma et répartir notre contenu en colonnes

--- src/components/GridColumns.vue
+++ src/components/GridColumns.vue
@@ -1,7 +1,25 @@
 <template>
   <!-- product info -->
   <section class="section">
-    
+    <div class="container">
+      <h1 class="title has-text-centered">Grid Columns</h1>
+      <div class="columns  is-vcentered">
+        <div class="column is-3">
+          <h1 class="is-size-1 title">Docker</h1>
+          <h2 class="is-size-2 subtitle">Dark Roast</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum rerum totam velit nobis ipsam odio ad ipsum omnis tempore amet!</p>
+        </div>
+        <div class="column is-5 has-text-centered">
+          <img src="../assets/product.png" alt="" class="px-6">
+        </div>
+        <div class="column is-4">
+          <h4 class="is-size-4 mb-4">
+            10.94€
+          </h4>
+          <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, accusamus.</p>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
